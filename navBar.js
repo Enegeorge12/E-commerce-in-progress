522a7cb7f2c8a3e8e14d9f9ac4bf70d379f663f6
Ene-George <ene.2456@gmail.com>
2022-05-11 11:51:19 +0300

    contact-page-delete11.05

--- navBar.js
+++ navBar.js
@@ -2,12 +2,12 @@ function myF(){
     let nav = document.getElementById("nav");
     if (nav.style.display === "flex") {
       nav.style.display = "none";
-      console.log("inchis");
+    
       
     } else {
       
       nav.style.display = "flex";
       nav.style.flexDirection="column"
-      console.log("deschis")
+      
     }
   }
