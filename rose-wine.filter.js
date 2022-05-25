9424f03354313bb5d7fdf5b6b5d057a6640caed2
Ene-George <ene.2456@gmail.com>
2022-05-25 12:08:09 +0300

    Title25.05

--- rose-wine.filter.js
+++ rose-wine.filter.js
@@ -621,7 +621,7 @@ let produse= [
           const a_details = document.createElement("a");
           a_details.classList.add("a-details");
           a_details.textContent = "Detalii";
-          a_details.href = '/product_details.html?id='+i.id;
+          a_details.href = 'product_details.html?id='+i.id;
         
           elem.appendChild(a_details);
         
